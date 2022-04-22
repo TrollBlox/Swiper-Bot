5ca80c1e58ebd4841e99cbad9392b76a72b3ad27
TrollBlox <caelbrenning@gmail.com>
2022-04-22 14:16:40 -0500

    all done

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import discord
+import json
+
+with open("config.json", "r") as read_file:
+  readConfig = json.load(read_file)
+
+class SwiperBot(discord.Client):
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+
+    if message.content.lower() == "yoink" and message.reference is not None:
+      msg = await message.channel.fetch_message(message.reference.message_id)
+      if msg.attachments:
+        channel = await message.author.create_dm()
+        for attachment in msg.attachments:
+          r = requests.get(attachment.url, allow_redirects = True)
+          open('0.png', 'wb').write(r.content)
+          await channel.send("```\n" + attachment.url + "\n```")
+
+client = SwiperBot()
+client.run(readConfig["token"])
